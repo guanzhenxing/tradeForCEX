5d1923f9544983f1858034a53f16ce4e2a718c5e
ybh <ybh@waterblock.io>
2023-02-01 14:32:08 +0800

    test

--- main.py
+++ main.py
@@ -4,13 +4,11 @@ client = Spot()
 
 # Get server timestamp
 print(client.time())
-# Get klines of BTCUSDT at 1m interval
-print(client.klines("BTCUSDT", "1m"))
-# Get last 10 klines of BNBUSDT at 1h interval
-print(client.klines("BNBUSDT", "1h", limit=10))
 
 # API key/secret are required for user data endpoints
 client = Spot(api_key=keys["binance_api_key"], api_secret=keys["binance_secret_key"])
 
 # Get account and balance information
 print(client.account())
+
+print(client.get_orders())
