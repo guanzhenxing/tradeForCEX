ee41095577e11545323ba240e7b3a11c6a9d1433
ybh <ybh@waterblock.io>
2023-02-14 09:22:23 +0800

    修复bug

--- backend/tool.py
+++ backend/tool.py
@@ -6,15 +6,15 @@ import time
 from config import keys
 
 
-def getRoundStatus(round):
+def getRoundStatus(count):
     '''
-    根据传入的round,查询当前round的订单状态是否已完成
-    返回1, 表示当前round还在运行。
-    返回0, 表示当前round已结束。
+    根据传入的count,查询当前count的订单状态是否已完成
+    返回1, 表示当前count还在运行。
+    返回0, 表示当前count已结束。
     '''
     mysql_client = OPMysql()
     sql = "SELECT order_id from trade_history where round={0} and done=0;"
-    res = mysql_client.op_select(sql.format(round))
+    res = mysql_client.op_select(sql.format(count))
     if len(res) > 0:
         return 1
     else:
@@ -37,30 +37,30 @@ def getRound():
     '''
     获取本轮策略交易编号
     '''
-    if redis_conn.exists("round"):
-        round = redis_conn.get("round")
+    if redis_conn.exists("count"):
+        count = int(redis_conn.get("count").decode("utf8"))
     else:
-        round = 0
-    return round
+        count = 0
+    return count
 
-def setRound(round):
+def setRound(count):
     '''
     设置策略交易编号
     '''
     try:
-        redis_conn.set("round", round)
-        logger.info("设置round为{0}".format(round))
+        redis_conn.set("count", count)
+        logger.info("设置count为{0}".format(count))
     except:
-        logger.info("设置round为{0}失败".format(round))
+        logger.info("设置count为{0}失败".format(count))
 
 
-def setLatestOpenBuyOrderId(round):
+def setLatestOpenBuyOrderId(count):
     '''
     将最新未成交买单塞进redis
     '''
     mysql_client = OPMysql()
     sql = "select order_id from trade_history where round={} and done=0 and id < 11 order by id asc limit 1;"
-    res_list =  mysql_client.op_select(sql.format(round))
+    res_list =  mysql_client.op_select(sql.format(count))
     # 如果结果为空
     if len(res_list) == 0:
         redis_conn.set("latest:open:buy:order_id", 0)
@@ -70,17 +70,17 @@ def setLatestOpenBuyOrderId(round):
         logger.info("设置最新未成交订单id: {0}".format(order_id))
 
 def getLatestOpenBuyOrderId():
-    order_id = redis_conn.get("latest:open:buy:order_id")
+    order_id = redis_conn.get("latest:open:buy:order_id").decode("utf8")
     return order_id
 
 
-def getSellOrderId(round):
+def getSellOrderId(count):
     '''
     获取卖单的订单id
     '''
     mysql_client = OPMysql()
     sql = "select order_id from trade_history where round = {0} and id = 11;"
-    res = mysql_client.op_select(sql.format(round))
+    res = mysql_client.op_select(sql.format(count))
     return res[0].get("order_id",None)
 
 def setOrderInfo(**kwargs):
@@ -88,22 +88,24 @@ def setOrderInfo(**kwargs):
     将订单信息入库
     '''
     mysql_client = OPMysql()
-    round = kwargs.get("round", None)
-    id = kwargs.get("id", None)
-    order_id = kwargs.get("order_id", None)
-    symbol = kwargs.get("symbol", None)
-    side = kwargs.get("side", None)
-    order_price = kwargs.get("order_price", None)
-    order_amount = kwargs.get("order_amount", None)
-    done = kwargs.get("done", None)
-    sql = "insert into trade_history (round, id, order_id, symbol, side, order_price, order_amount, done) values ({0}, {1}, {2}, {3}, {4}, {5}, {6}, {7});"
+    count = kwargs.get("count", 'NULL')
+    id = kwargs.get("id", 'NULL')
+    order_id = kwargs.get("order_id", 'NULL')
+    symbol = kwargs.get("symbol", 'NULL')
+    side = kwargs.get("side", 'NULL')
+    order_price = kwargs.get("order_price", 'NULL')
+    order_amount = kwargs.get("order_amount", 'NULL')
+    done = kwargs.get("done", 0)
+    sql = "insert into trade_history (`round`, `id`, `order_id`, `symbol`, `side`, `order_price`, `order_amount`, `done`) values ({0}, {1}, {2}, '{3}', {4}, {5}, {6}, {7});"
     while True:
         try:
-            res = mysql_client.op_insert(sql.format(round, id, order_id, symbol, side, order_price, order_amount, done))
+            logger.info(sql.format(count, id, order_id, symbol, side, order_price, order_amount, done))
+            res = mysql_client.op_insert(sql.format(count, id, order_id, symbol, side, order_price, order_amount, done))
             if res:
                 logger.info("订单：%s 入库成功！" % order_id)
                 break
-        except:
+        except Exception as e:
+            logger.exception(e)
             time.sleep(1)
 
 def updateOrderInfo(**kwargs):
@@ -111,17 +113,17 @@ def updateOrderInfo(**kwargs):
     更新一个订单信息
     '''
     mysql_client = OPMysql()
-    order_id = kwargs.get("order_id", None)
-    order_amount = kwargs.get("order_amount", None)
-    done = kwargs.get("done", None)
+    order_id = kwargs.get("order_id", 'NULL')
+    order_amount = kwargs.get("order_amount", 'NULL')
+    done = kwargs.get("done", 'NULL')
     sql = "update trade_history set order_amount = {1}, done = {2} where order_id={0};"
     while True:
         try:
-            res = mysql_client.op_insert(sql.format(order_id, order_amount, done))
-            if res:
-                logger.info("订单: %s 更新成功！" % order_id)
-                break
-        except:
+            mysql_client.op_insert(sql.format(order_id, order_amount, done))
+            logger.info("订单: %s 更新成功！" % order_id)
+            break
+        except Exception as e:
+            logger.exception(e)
             time.sleep(1)
 
 def delOrderInfo(order_id):
@@ -172,26 +174,25 @@ def cancelReplaceOrder(order_id, quantity, price):
     '''
     撤销订单并重新下单
     '''
-    params = {
-            "symbol": keys["symbol"],
-            "side": "SELL",
-            "type": "LIMIT",
-            "timeInForce": "GTC",
-            "quantity": quantity,
-            "price": price,
-            "cancelReplaceMode": "STOP_ON_FAILURE",
-            "cancelOrderId": order_id
-        }
-    res = client.cancel_and_replace(**params)
+    # 撤销订单
+    while True:
+        res = getOrderStatus(order_id)
+        if res["status"] == "FILLED":
+            break
+        else:
+            canccelOrder(order_id)
+    
+    # 重新下单
+    res = createLimitOrder(1, quantity, price)
     return res
 
-def reCalTakeProfitOrder(round):
+def reCalTakeProfitOrder(count):
     '''
     重新计算止盈单的挂单信息
     '''
     mysql_client = OPMysql()
-    sql = "select order_price, order_amount from trade_history where round = {0};"
-    res_list = mysql_client.op_select(sql.format(round))
+    sql = "select order_price, order_amount from trade_history where round = {0} and done = 1 and side = 0;"
+    res_list = mysql_client.op_select(sql.format(count))
     # 汇总交易所得的btc总量
     total_btc_amount = 0
     # 汇总交易花费的usd总量
@@ -201,7 +202,7 @@ def reCalTakeProfitOrder(round):
         total_usd_amount += res["order_price"] * res["order_amount"]
     # 重新计算卖单价格
     new_sell_price = total_usd_amount * (1 + keys["profit_target"]) / total_btc_amount
-    return new_sell_price, total_btc_amount
+    return round(new_sell_price,2), round(total_btc_amount,4)
 
 
 
@@ -216,11 +217,26 @@ def canccelOrder(order_id):
     res = client.cancel_order(**params)
     return res
 
-def getAllOpenOrderId(round):
+def getAllOpenOrderId(count):
     '''
     获取所有未成交订单id
     '''
     mysql_client = OPMysql()
     sql = "select order_id from trade_history where round={} and done=0 and id < 11;"
-    res_list =  mysql_client.op_select(sql.format(round))
+    res_list =  mysql_client.op_select(sql.format(count))
     return res_list
+
+
+def cancelOpenOrders(symbol):
+    params = {
+            "symbol": keys["symbol"]
+        }
+    res = client.cancel_open_orders(**params)
+    return res
+
+def getOpenOrders(symbol):
+    params = {
+            "symbol": keys["symbol"]
+        }
+    res = client.get_open_orders(**params)
+    return res
--- main.py
+++ main.py
@@ -17,6 +17,8 @@ from backend.tool import getSellOrderId
 from backend.tool import delOrderInfo
 from backend.tool import canccelOrder
 from backend.tool import getAllOpenOrderId
+from backend.tool import cancelOpenOrders
+from backend.tool import getOpenOrders
 from multiprocessing import Process
 import time
 import math
@@ -30,6 +32,7 @@ def checkLatestOpenBuyOder():
         try:
             order_id = getLatestOpenBuyOrderId()
             res = getOrderStatus(order_id)
+            logger.info(res)
             if res["status"] == "FILLED":
                 # 获得btc数量
                 executedQty = res["executedQty"]
@@ -37,23 +40,26 @@ def checkLatestOpenBuyOder():
                 # 更新订单信息
                 updateOrderInfo(**order_info)
                 # 重新计算止盈单订单信息
-                round = getRound()
-                new_price, new_quantity = reCalTakeProfitOrder(round)
+                logger.info("1")
+                count = getRound()
+                new_price, new_quantity = reCalTakeProfitOrder(count)
+                logger.info(new_price)
                 # 撤销订单并重新下单
-                sell_order_id = getSellOrderId(round)
+                sell_order_id = getSellOrderId(count)
+                logger.info(sell_order_id)
                 res = cancelReplaceOrder(sell_order_id, new_quantity, new_price)
-                if res.get("code", None) == None:
-                    # 撤销订单并重新下单成功
-                    new_sell_order_id = res["newOrderResponse"]["orderId"]
-                    delOrderInfo(sell_order_id)
-                    new_order_info = {"round": round, "id": 11, "order_id": new_sell_order_id, "symbol": keys["symbol"], "side": 1, "order_price": new_price, "done": 0}
-                    setOrderInfo(**new_order_info)
-                    logger.info("止盈单更新成功！")
-                    # 更新最新未成交订单
-                    setLatestOpenBuyOrderId(round)
+                logger.info(res)
+                # 撤销订单并重新下单成功
+                new_sell_order_id = res["orderId"]
+                delOrderInfo(sell_order_id)
+                new_order_info = {"count": count, "id": 11, "order_id": new_sell_order_id, "symbol": keys["symbol"], "side": 1, "order_price": new_price, "done": 0}
+                setOrderInfo(**new_order_info)
+                logger.info("止盈单更新成功！")
+                # 更新最新未成交订单
+                setLatestOpenBuyOrderId(count)
         except Exception as e:
             logger.exception(e)
-        time.sleep(1)
+        time.sleep(5)
 
             
 
@@ -66,9 +72,9 @@ def checkOpenSellOder():
     '''
     while True:
         try:
-            round = getRound()
-            if round:
-                sell_order_id = getSellOrderId(round)
+            count = getRound()
+            if count:
+                sell_order_id = getSellOrderId(count)
                 if sell_order_id is not None:
                     res = getOrderStatus(sell_order_id)
                     if res["status"] == "FILLED":
@@ -77,7 +83,7 @@ def checkOpenSellOder():
                         # 更新订单信息
                         updateOrderInfo(**order_info)
                         # 获取所有未成交订单id
-                        res_list = getAllOpenOrderId(round)
+                        res_list = getAllOpenOrderId(count)
                         for res in res_list:
                             # 撤单
                             r = canccelOrder(res["order_id"])
@@ -86,18 +92,18 @@ def checkOpenSellOder():
                             delOrderInfo(res["order_id"])
                             time.sleep(1)
                         # 开启新一轮策略
-                        startRound(round+1)
-                        logger.info("开启第{0}轮策略！".format(round+1))
+                        startRound(count+1)
+                        logger.info("开启第{0}轮策略！".format(count+1))
         except Exception as e:
             logger.exception(e)
-        time.sleep(1)
+        time.sleep(5)
 
-def startRound(round):
+def startRound(count):
     '''
     开启新的一轮策略
     '''
     # 设置轮次
-    setRound(round)
+    setRound(count)
     # 创建第一个市价单
     res = createFirstOrder()
     status = res["status"]
@@ -116,14 +122,14 @@ def startRound(round):
     # 交易的BTC平均价格
     avg_price = float(cummulativeQuoteQty) / float(executedQty)
     # 首次订单id为0
-    order_info = {"round": round, "id": 0, "order_id": first_order_id, "symbol": keys["symbol"], "side": 0, "order_price": avg_price, "order_amount": cummulativeQuoteQty, "done": 1}
+    order_info = {"count": count, "id": 0, "order_id": first_order_id, "symbol": keys["symbol"], "side": 0, "order_price": round(avg_price,2), "order_amount": round(float(executedQty),4), "done": 1}
     setOrderInfo(**order_info)
 
     # 开始生成加仓单
-    count = 1
-    total_buy_order_busd_amount = cummulativeQuoteQty
+    times = 1
+    total_buy_order_busd_amount = float(cummulativeQuoteQty)
     order_price = avg_price
-    while count <= 10:
+    while times <= 10:
         buy_order_usd_amount = float(keys["first_order_usd"]) * math.pow(keys["multiplier_of_position_increase_amount"], count - 1)
         quantity = buy_order_usd_amount / (order_price * (1 - keys["down_how_much_to_add_to_the_position"]))
         order_price = order_price * (1 - keys["down_how_much_to_add_to_the_position"])
@@ -131,22 +137,23 @@ def startRound(round):
         total_buy_order_busd_amount += buy_order_usd_amount
         if total_buy_order_busd_amount > keys["limit_usd"]:
             break
-        res = createLimitOrder("BUY", quantity, order_price)
+        res = createLimitOrder("BUY", round(quantity,4), round(order_price,2))
         logger.info("挂一个加仓单, 返回信息: {0}".format(res))
-        order_info = {"round": round, "id": count, "order_id": res["orderId"], "symbol": keys["symbol"], "side": 0, "order_price": order_price, "done": 0}
+        order_info = {"count": count, "id": times, "order_id": res["orderId"], "symbol": keys["symbol"], "side": 0, "order_price": round(order_price,2), "done": 0}
         setOrderInfo(**order_info)
+        times += 1
     
     # 开始生成止盈单
     sell_price = avg_price * (1 + keys["profit_target"])
-    sell_quantity = executedQty
-    res = createLimitOrder("SELL", sell_quantity, sell_price)
+    sell_quantity = float(executedQty)
+    res = createLimitOrder("SELL", round(sell_quantity,4), round(sell_price,2))
     logger.info("挂一个止盈单, 返回信息: {0}".format(res))
     # 止盈单的id为11
-    order_info = {"round": round, "id": 11, "order_id": res["orderId"], "symbol": keys["symbol"], "side": 1, "order_price": sell_price, "done": 0}
+    order_info = {"count": count, "id": 11, "order_id": res["orderId"], "symbol": keys["symbol"], "side": 1, "order_price": round(sell_price,2), "done": 0}
     setOrderInfo(**order_info)
 
     # 设置最新未成交订单
-    setLatestOpenBuyOrderId(round)
+    setLatestOpenBuyOrderId(count)
     
     
     
@@ -159,16 +166,29 @@ def main():
          2) 还在运行则跳过
     如果没有，则开启新一轮策略
     '''
-    round = getRound()
-    if round > 0:
-        round_running = getRoundStatus(round)
-        if not round_running:
-            new_round = int(round) + 1
-            startRound(new_round)
+    count = getRound()
+    if count > 0:
+        count_running = getRoundStatus(count)
+        if not count_running:
+            new_count = int(count) + 1
+            startRound(new_count)
     else:
         startRound(1)
 
 
 if __name__ == "__main__":
     logger.info("量化机器人启动！")
+    # main()
+    # p1 = Process(target=checkLatestOpenBuyOder)
+    # p1.start()
+    # logger.info("检测最新买单成交！")
+    # p2 = Process(target=checkOpenSellOder)
+    # p2.start()
+    # logger.info("检测止盈订单成交！")
+    # res = cancelOpenOrders(keys["symbol"])
+    # logger.info(res)
+    res = getOpenOrders(keys["symbol"])
+    logger.info(res)
+    res =  getOrderStatus(3843464)
+    logger.info(res)
     
